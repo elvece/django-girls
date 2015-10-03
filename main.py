77ad195164d6fd7825eb526c8b1e9629f5fd9e74
elvece <lvcifferello@gmail.com>
2015-10-03 10:28:35 -0600

    practicing with if, elif, and else

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 if 3 > 2:
-  print ('It works!')
+  print ('3 is greater than 2')
+else:
+  print ('3 is not greater than 2')
+
+name = 'Lucy'
+if (name == 'Sonja'):
+  print ('Hello Sonja')
+elif (name == 'Lucy'):
+  print ('Hello Lucy')
+else:
+  print ('Hey you')
+
+score = 90
+if (0 <= score <= 40):
+  print ('You Failed!')
+elif (41 <= score <= 60):
+  print ('You passed with a below average score')
+elif (61 <= score <= 80):
+  print ('You passed!')
+else:
+  print ('You passed with an above average score')
